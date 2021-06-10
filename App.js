a7d3e2697378fcd736aa63f1d5d567efb3d109c8
Alpish Jain <jalpish@gmail.com>
2021-06-10 11:57:17 +0530

    Changes in overview section

---
+++ App.js
@@ -0,0 +1,124 @@
+import { StatusBar } from 'expo-status-bar';
+import React ,{useState,useEffect} from 'react';
+import { StyleSheet, Text, View , Button} from 'react-native';
+import Box from './components/box';
+import { Ionicons } from '@expo/vector-icons'; 
+
+export default function App() {
+ 
+const [boxes,setBoxes]=useState(Array(9).fill(null));
+const [isXChance,setIsXChance]=useState(true);
+const [winner,setWinner]=useState(null);
+const [history,setHistory]=useState([boxes,]);
+
+function PlayBox(no){
+  return(
+  <Box no={no} boxInfo={{boxes,setBoxes}} chance={{isXChance,setIsXChance}} winner={winner} />
+  );
+}
+function calculateWinner() {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (boxes[a]!=null && boxes[a] && boxes[a] === boxes[b] && boxes[a] === boxes[c]) {
+      setWinner(boxes[a]); 
+      return ;
+    }
+  }
+  return null;
+}
+
+useEffect(()=>{
+  calculateWinner();
+
+},[isXChance])
+
+
+function Boxes(){
+  return (
+    <View style={styles.playBoard}>
+    <View style={styles.rows}>
+      {PlayBox(0)}
+      {PlayBox(1)}
+      {PlayBox(2)}
+    </View>
+    <View style={styles.rows}>
+      {PlayBox(3)}
+      {PlayBox(4)}
+      {PlayBox(5)}
+    </View>
+    <View style={styles.rows}>
+      {PlayBox(6)}
+      {PlayBox(7)}
+      {PlayBox(8)}
+    </View>
+    </View>
+  );
+}
+
+
+
+  return (
+    <View style={styles.container}>
+        <StatusBar style="auto"  backgroundColor='blue'/>
+        <View style={styles.featureContainer}>{
+            winner!=null?
+            <Text style={[styles.primaryText,styles.winnerText]}>{winner} Won!</Text>:
+            <Text styles={styles.primaryText}>Chance:{isXChance?'X':'O'}</Text>
+}
+
+</View>
+    <Boxes />
+    </View>
+  );
+}
+
+
+
+const styles=StyleSheet.create({
+container:{
+  flex:1,
+  backgroundColor:'#fff',
+  justifyContent:'center',
+  alignItems:'center',
+},
+
+  playBoard:{
+    borderWidth:5,
+    borderRadius:10,
+
+  },
+  resetIcon: {
+    position: 'absolute',
+    right: 20,
+  },
+  rows:{
+    flexDirection:'row'
+  },
+  featureContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+    marginBottom: 20,
+  },
+  primaryText: {
+    fontSize: 40,
+    color: 'black',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  
+  },
+  winnerText: {
+    color: 'darkorange',
+    fontSize: 48,
+  }
+});
